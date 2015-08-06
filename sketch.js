1d44c1273fccbc7025a74da0e4fecab627aea6d9
C4mz0r <aut0x@hotmail.com>
2015-08-05 20:14:27 -0600

    add resize and shake capabilities

--- sketch.js
+++ sketch.js
@@ -3,57 +3,73 @@ var numcols = 10;
 
 $(document).ready(
 	
-	function(){
 
-		alert('drawing');
-
-		// determine how many rows / columns of pixels we will have
-		//var numrows = 10;
-		//var numcols = 10;
+	function (){
+		drawSquares();		
 	
-		// draw them out a row at a time
-		for (var rows = 0; rows < numrows; rows++){
-			$('#main').append("<div class='rowstart'></div>");
-			for (var cols = 0; cols < numcols; cols++){
-				$('#main').append("<div class='pixel'></div>");
-			}			
-		}
-
-		// determine how big each "pixel" should be on screen by finding out the total dimensions from the parent container.
-		// I found that I needed to subtract 1px off of each square to make it fit perfectly into the box, otherwise it would render the last box of each row on to the next line.  
-		// I think it may have to do with the fact that I have 1px border on each item and I've already done a 1px shift left (and down), leaving me with 1px on the right of each box. 
-		// Anyway, this seems to do the trick.
-		var pixelHeight = parseInt($("#main").css("height")) / numrows - 1;
-		var pixelWidth = parseInt($("#main").css("width")) / numcols - 1;
 		
-		// Setting the pixel height / width needed to be done after the pixels were drawn, it didn't work if done before.		
-		$('.pixel').css("height", pixelHeight);
-		$('.pixel').css('width', pixelWidth);
+		// This will be called if the shake button is pressed
+		$("#shake").click(function(){
+			$("#main").effect("bounce", {times:2, direction:"down"}, "slow", function(){
+				$("body").fadeOut("slow", function(){
+					drawSquares();	
+				});
+			});
+			
+		});
+	
 
-
-		// when hovered over, change the highlighting of the current pixel
-		$('.pixel').hover(
-			function(){
-				$(this).addClass('highlighted')				
-			}
-		);
 	}
-
-
 );
 
-function reset()
-{
-	numcols = numrows = prompt('Enter the number of pixels along each edge');
-	console.log(numcols);
-	//location.reload();
-	$('body').empty();
-	jQuery.ready();
-	
-	
+
+// Define and draw out the div squares and how the hovering will work.
+function drawSquares(){		
+
+	$('#main').empty();
+	$('body').fadeIn();
+	// draw them out a row at a time
+	for (var rows = 0; rows < numrows; rows++){
+		$('#main').append("<div class='rowstart'></div>");
+		for (var cols = 0; cols < numcols; cols++){
+			$('#main').append("<div class='pixel'></div>");
+		}			
+	}
+		
+	var pixelHeight = parseInt($("#main").css("height")) / numrows - 1;
+	var pixelWidth = parseInt($("#main").css("width")) / numcols - 1;
+
+	// Setting the pixel height / width needed to be done after the pixels were drawn, it didn't work if done before.		
+	$('.pixel').css("height", pixelHeight);
+	$('.pixel').css('width', pixelWidth);
+
+
+	// when hovered over, change the highlighting of the current pixel
+	$('.pixel').hover(
+		function(){
+			$(this).addClass('highlighted')				
+		}
+	);
 }
 
-function b()
+// Prompt for the edge, and then draws out a new grid of that dimension.
+function reset()
 {
-	alert('hi');
+	var oldValue = numcols;
+	numcols = numrows = parseInt(prompt('Enter the number of pixels (1-100):'));	
+
+	if ( !isNaN(numcols) && numcols > 0 && numcols <= 100)
+	{
+		// it is OK
+	}
+	else
+	{
+		alert("You must enter an integer between 0 and 100.");	
+		numcols = oldValue;
+		numrows = oldValue;
+	}
+
+	drawSquares();	
 }
+
+
